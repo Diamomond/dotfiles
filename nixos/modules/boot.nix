b48d9098f088e522b046e61364500f9628deb7fc
Diamomond <88452621+Diamomond@users.noreply.github.com>
2026-06-13 07:05:49 +0200

    Update boot.nix

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -10,7 +10,10 @@
     "zswap.enabled=1" # enables zswap
     "zswap.compressor=zstd" # compression algorithm
     "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
-    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+    "zswap.shrinker_enabled=1"# whether to shrink the pool proactively on high memory pressure
+    "i8042.nopnp"
+    "i8042.direct"
+    "i8042.reset"
   ];
   
 
