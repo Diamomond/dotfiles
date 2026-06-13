2e2913e6def5603d5261b4152c4956b1fe57c6fa
Diamomond <88452621+Diamomond@users.noreply.github.com>
2026-06-13 07:05:35 +0200

    Update cachyos.nix

--- nixos/modules/cachyos.nix
+++ nixos/modules/cachyos.nix
@@ -5,7 +5,7 @@
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
 
   nix.settings = {
-    substituters = [ "https://attic.xuyh0120.win/lantian" ];
-    trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+    substituters = [ "https://attic.xuyh0120.win/lantian" "https://noctalia.cachix.org" ];
+    trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
   };
 }
